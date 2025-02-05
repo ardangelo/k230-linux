145e98efe03c279785944e5c20f6cdb8caca5761
wangjianxin <wangjianxin@canaan-creative.com>
2025-02-05 16:47:01 +0800

    01 default enable rtsp_demo

--- buildroot-overlay/package/camera_rtsp_demo/camera_rtsp_demo.mk
+++ buildroot-overlay/package/camera_rtsp_demo/camera_rtsp_demo.mk
@@ -2,6 +2,8 @@ CAMERA_RTSP_DEMO_SITE = $(realpath $(TOPDIR))"/package/camera_rtsp_demo/src"
 CAMERA_RTSP_DEMO_SITE_METHOD = local
 CAMERA_RTSP_DEMO_INSTALL_STAGING = YES
 
+CAMERA_RTSP_DEMO_DEPENDENCIES += librtsp_server ffmpeg
+
 define CAMERA_RTSP_DEMO_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D)
 endef
